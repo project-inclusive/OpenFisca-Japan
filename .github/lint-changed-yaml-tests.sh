d2e01ca29f1d415803de9948139d30b2420cd711
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2023-04-30 11:41:16 +0900

    Revert "proj-inclusive/OpenFisca-Shibuya (725d65d, 2023/4/29)をコピー"
    This reverts commit 489a476119f69989c2ac447d835e1cf82a145cc7.

--- .github/lint-changed-yaml-tests.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-if ! files=$(find openfisca_yuisekin|grep yaml)
-then
-  echo "Linting the following YAML files:"
-  echo $files
-  yamllint $files
-else echo "No YAML files to lint"
-fi
