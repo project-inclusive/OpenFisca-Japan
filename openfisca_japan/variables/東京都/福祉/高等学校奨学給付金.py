0083fa09a0633056c2ee9e1c995ea43c9addb418
he23inw3 <golden2finger@gmail.com>
2023-08-27 15:27:08 +0900

    高等学校奨学給付金の実装追加

--- openfisca_japan/variables/東京都/福祉/高等学校奨学給付金.py
+++ openfisca_japan/variables/東京都/福祉/高等学校奨学給付金.py
@@ -4,7 +4,7 @@
 
 import csv
 from enum import Enum as OriginalEnum
-
+import math
 import numpy as np
 
 from openfisca_core.periods import DAY
@@ -24,10 +24,12 @@ class 高等学校奨学給付金_最小(Variable):
     label = "高等学校奨学給付金"
     reference = "https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html"
     documentation = """
-    高等学校奨学給付金_最小(年齢を降順ソートされた状態で送られてくる想定)
+    高等学校奨学給付金_最小
     """
 
     def formula(対象世帯, 対象期間, parameters):
+        # NOTE: 年齢が降順ソートされた状態で送られてくる想定
+
         居住都道府県 = 対象世帯("居住都道府県", 対象期間)
         if 居住都道府県[0] != "東京都":
             return 0
@@ -37,16 +39,16 @@ class 高等学校奨学給付金_最小(Variable):
         if 高校生の人数 == 0:
             return 0
 
-        給付金 = 0
+        年間支給金額 = 0
         生活保護受給可能 = 対象世帯("生活保護", 対象期間) > 0
         if 生活保護受給可能:
-            高校種別 = 対象世帯.members("高校種別", 対象期間).decode()
-            高校種別 = 高校種別[高校種別 != 高校種別パターン.無]
+            高校種別一覧 = 対象世帯.members("高校種別", 対象期間).decode()
+            高校種別一覧 = 高校種別一覧[高校種別一覧 != 高校種別パターン.無]
 
-            for i in 高校種別:
-                給付金 += int(高等学校奨学給付金表["生活保護（生業扶助）受給世帯"][i.value])
+            for 高校種別 in 高校種別一覧:
+                年間支給金額 += int(高等学校奨学給付金表["生活保護（生業扶助）受給世帯"][高校種別.value])
         
-        return 月額支給金額へ変換(給付金)
+        return 月間支給金額へ変換(年間支給金額)
 
 class 高等学校奨学給付金_最大(Variable):
     value_type = int
@@ -55,10 +57,12 @@ class 高等学校奨学給付金_最大(Variable):
     label = "高等学校奨学給付金"
     reference = "https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html"
     documentation = """
-    高等学校奨学給付金_最大(年齢を降順ソートされた状態で送られてくる想定)
+    高等学校奨学給付金_最大
     """
 
     def formula(対象世帯, 対象期間, parameters):
+        # NOTE: 年齢が降順ソートされた状態で送られてくる想定
+
         居住都道府県 = 対象世帯("居住都道府県", 対象期間)
         if 居住都道府県[0] != "東京都":
             return 0
@@ -72,21 +76,21 @@ class 高等学校奨学給付金_最大(Variable):
         if not 住民税非課税世帯:
             return 0
 
-        給付金 = 0
-        高校種別 = 対象世帯.members("高校種別", 対象期間).decode()
-        高校種別 = 高校種別[高校種別 != 高校種別パターン.無]
+        年間支給金額 = 0
+        高校種別一覧 = 対象世帯.members("高校種別", 対象期間).decode()
+        高校種別一覧 = 高校種別一覧[高校種別一覧 != 高校種別パターン.無]
 
-        for (i, j) in enumerate(高校種別):
+        for (i, 高校種別) in enumerate(高校種別一覧):
             if i == 0: # 第一子
-                if np.any(高校種別 == 高校種別パターン.通信制課程):
+                if np.any(高校種別一覧 == 高校種別パターン.通信制課程) and 高校種別 == 高校種別パターン.全日制:
                     # 当該世帯に通信制過程の高等学校等に在籍する高校生がいる場合の、非課税世帯(第一子)の支給金額
-                    給付金 += parameters(対象期間).東京都.福祉.高等学校奨学給付金.非課税世帯第一子の特別支給金額
+                    年間支給金額 += parameters(対象期間).東京都.福祉.高等学校奨学給付金.非課税世帯第一子の特別支給金額
                 else:
-                    給付金 += int(高等学校奨学給付金表["非課税世帯（第1子）"][j.value])
+                    年間支給金額 += int(高等学校奨学給付金表["非課税世帯（第1子）"][高校種別.value])
             else: # 第二子以降
-                給付金 += int(高等学校奨学給付金表["非課税世帯（第2子）"][j.value])
+                年間支給金額 += int(高等学校奨学給付金表["非課税世帯（第2子）"][高校種別.value])
 
-        return 月額支給金額へ変換(給付金)
+        return 月間支給金額へ変換(年間支給金額)
 
 class 高校種別パターン(Enum):
     __order__ = "無 全日制 定時制課程 通信制課程 専攻科"
@@ -109,5 +113,5 @@ class 高校生学年(OriginalEnum):
     二年生 = 11
     三年生 = 12
 
-def 月額支給金額へ変換(年額):
-    return int(年額 / 12)
+def 月間支給金額へ変換(年間支給金額):
+    return math.floor(年間支給金額 / 12)
