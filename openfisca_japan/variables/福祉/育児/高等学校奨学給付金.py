1eeaf48a5f825e9ae563dd2c51f317e5415227c8
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2023-09-04 23:06:31 +0900

    高等学校奨学給付金が東京都以外でも算出されるようにした。

--- openfisca_japan/variables/福祉/育児/高等学校奨学給付金.py
+++ openfisca_japan/variables/福祉/育児/高等学校奨学給付金.py
@@ -12,7 +12,7 @@ from openfisca_core.variables import Variable
 from openfisca_japan.entities import 世帯, 人物
 from openfisca_core.indexed_enums import Enum
 
-with open('openfisca_japan/parameters/東京都/福祉/高等学校奨学給付金/高等学校奨学給付金額.csv') as f:
+with open('openfisca_japan/parameters/福祉/育児/高等学校奨学給付金/高等学校奨学給付金額.csv') as f:
     reader = csv.DictReader(f)
     # 高等学校奨学給付金表[世帯区分][履修形態] の形で参照可能
     高等学校奨学給付金表 = {row[""]: row for row in reader}
@@ -22,9 +22,10 @@ class 高等学校奨学給付金_最小(Variable):
     entity = 世帯
     definition_period = DAY
     label = "高等学校奨学給付金"
-    reference = "https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html"
+    reference = "https://www.mext.go.jp/a_menu/shotou/mushouka/1344089.htm"
     documentation = """
     高等学校奨学給付金_最小
+    (東京都HP)https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html
     """
 
     def formula(対象世帯, 対象期間, parameters):
@@ -43,9 +44,10 @@ class 高等学校奨学給付金_最大(Variable):
     entity = 世帯
     definition_period = DAY
     label = "高等学校奨学給付金"
-    reference = "https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html"
+    reference = "https://www.mext.go.jp/a_menu/shotou/mushouka/1344089.htm"
     documentation = """
     高等学校奨学給付金_最大
+    (東京都HP)https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html
     """
 
     def formula(対象世帯, 対象期間, parameters):
@@ -64,16 +66,12 @@ class 生活保護受給世帯の高等学校奨学給付金(Variable):
     entity = 世帯
     definition_period = DAY
     label = "生活保護受給世帯の高等学校奨学給付金"
-    reference = "https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html"
+    reference = "https://www.mext.go.jp/a_menu/shotou/mushouka/1344089.htm"
     documentation = """
     生活保護受給世帯の高等学校奨学給付金
     """
     
     def formula(対象世帯, 対象期間, parameters):
-        居住都道府県 = 対象世帯("居住都道府県", 対象期間)
-        if 居住都道府県[0] != "東京都":
-            return 0
-
         年齢一覧 = 対象世帯.members("年齢", 対象期間)
         学年一覧 = 対象世帯.members("学年", 対象期間)
         子供である = 対象世帯.has_role(世帯.子)
@@ -101,16 +99,12 @@ class 住民税非課税世帯の高等学校奨学給付金(Variable):
     entity = 世帯
     definition_period = DAY
     label = "住民税非課税世帯の高等学校奨学給付金"
-    reference = "https://www.kyoiku.metro.tokyo.lg.jp/admission/tuition/tuition/scholarship_public_school.html"
+    reference = "https://www.mext.go.jp/a_menu/shotou/mushouka/1344089.htm"
     documentation = """
     住民税非課税世帯の高等学校奨学給付金
     """
 
     def formula(対象世帯, 対象期間, parameters):
-        居住都道府県 = 対象世帯("居住都道府県", 対象期間)
-        if 居住都道府県[0] != "東京都":
-            return 0
-
         子供である = 対象世帯.has_role(世帯.子)
         年齢一覧 = 対象世帯.members("年齢", 対象期間)
         学年一覧 = 対象世帯.members("学年", 対象期間)
@@ -136,7 +130,7 @@ class 住民税非課税世帯の高等学校奨学給付金(Variable):
             if i == 0: # 第一子目
                 if np.any(高校種別一覧 == 高校種別パターン.通信制課程) and (高校種別 in [高校種別パターン.全日制課程, 高校種別パターン.定時制課程]):
                     # 当該世帯に通信制過程の高等学校等に在籍する高校生がいる場合の、非課税世帯(第一子)の支給金額
-                    年間支給金額 += parameters(対象期間).東京都.福祉.高等学校奨学給付金.非課税世帯第一子の特別支給金額
+                    年間支給金額 += parameters(対象期間).福祉.育児.高等学校奨学給付金.非課税世帯第一子の特別支給金額
                 else:
                     年間支給金額 += int(高等学校奨学給付金表["非課税世帯（第1子）"][高校種別.value])
             else: # それ以降
