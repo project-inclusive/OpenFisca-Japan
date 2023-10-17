0bedf2423d49942cf26c1dde8bbe45814cf457c1
he23inw3 <golden2finger@gmail.com>
2023-10-17 19:15:07 +0900

    「自分」「配偶者」「子」の預貯金だけ見るように修正

--- openfisca_japan/variables/預貯金.py
+++ openfisca_japan/variables/預貯金.py
@@ -1,3 +1,6 @@
+
+import numpy as np
+
 from openfisca_core.periods import DAY
 from openfisca_core.variables import Variable
 from openfisca_japan.entities import 世帯, 人物
@@ -9,3 +12,19 @@ class 預貯金(Variable):
     definition_period = DAY
     label = "預貯金"
     reference = "https://www.fukushi.metro.tokyo.lg.jp/seikatsu/teisyotokusyataisaku/jukenseichallenge.html"
+
+class 子供の預貯金(Variable):
+    value_type = int
+    default_value = 0
+    entity = 世帯
+    definition_period = DAY
+    label = "子供の預貯金"
+    reference = "https://www.fukushi.metro.tokyo.lg.jp/seikatsu/teisyotokusyataisaku/jukenseichallenge.html"
+
+    def formula(対象世帯, 対象期間, parameters):
+        if 対象世帯.nb_persons(世帯.子) == 0:
+            return 0
+
+        子供である = 対象世帯.has_role(世帯.子)
+        預貯金一覧 = 対象世帯.members("預貯金", 対象期間)
+        return np.sum(預貯金一覧[子供である])
