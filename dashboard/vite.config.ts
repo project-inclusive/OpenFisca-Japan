ce786d549949476fc2809ff7c5eac6de7dcc08e5
Kazuya AZEGAMI <k-azegami@youthfuldays.co.jp>
2023-11-02 21:55:22 +0900

    Google Analyticsを環境によって切り替えられるように、IDを環境変数化した

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.GITHUB_PAGES
-    ? 'OpenFisca-Japan' // レポジトリ名を設定
-    : '/',
-});
+export default () => {
+
+  // index.htmlの「%=」「%」で囲われた部分を環境変数値で置換する
+  //   例: <title>%=ENV_NAME%</title> -> <title>ENV_NAME</title>
+  const env = { ...process.env };
+  const htmlPlugin = () => ({
+    name: 'html-transform',
+    transformIndexHtml: (html: string): string =>
+      html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
+  });
+
+  return defineConfig({
+    plugins: [
+      react(),
+      htmlPlugin(),
+    ],
+    base: process.env.GITHUB_PAGES
+      ? 'OpenFisca-Japan' // レポジトリ名を設定
+      : '/',
+  });
+};
