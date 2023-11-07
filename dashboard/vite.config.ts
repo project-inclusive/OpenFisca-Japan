daf19a3f12c384df14783ab7828833e0de4d6d36
Kazuya AZEGAMI <k-azegami@youthfuldays.co.jp>
2023-11-07 11:26:58 +0000

    エラーになっていたvite.config.tsフォーマットを修正した

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default () => {
-
   // index.htmlの「%=」「%」で囲われた部分を環境変数値で置換する
   //   例: <title>%=ENV_NAME%</title> -> <title>ENV_NAME</title>
   const env = { ...process.env };
@@ -14,10 +13,7 @@ export default () => {
   });
 
   return defineConfig({
-    plugins: [
-      react(),
-      htmlPlugin(),
-    ],
+    plugins: [react(), htmlPlugin()],
     base: process.env.GITHUB_PAGES
       ? 'OpenFisca-Japan' // レポジトリ名を設定
       : '/',
