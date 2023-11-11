c466e398659b4eaeaf99858df604e5985b18d0c4
minosys-jp <minoru@minosys.com>
2023-11-11 10:24:25 +0900

    eslint recovered

---
+++ dashboard/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier'
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "tsconfigRootDir": __dirname,
+	"project": ['./tsconfig.json']
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off'
+    },
+    "ignorePatterns": [
+        'build/',
+        'public/,',
+        '**/node_modules/',
+        '*.config.js',
+        '.*lintrc.js',
+        '/*.*'
+    ]
+}
