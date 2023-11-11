6be2c20178cc8e42780c9106822df967393c86db
mu-perori <mu17perori@gmail.com>
2023-11-11 23:03:28 +0900

    Update update_city_json.py

--- dashboard/src/config/develop/file_handling.py
+++ dashboard/src/config/develop/file_handling.py
@@ -8,7 +8,7 @@ import json
 # CSVファイルの読み込み
 def read_csv(file_name, encoding_format):
 	return_list = []
-	with open(f'result/{file_name}.csv', 'r', encoding=encoding_format) as f:
+	with open(f'{file_name}.csv', 'r', encoding=encoding_format) as f:
 		reader = csv.reader(f)
 		for row in reader:
 			return_list.append(row)
@@ -17,15 +17,19 @@ def read_csv(file_name, encoding_format):
 # CSVファイルの書き出し
 def make_csv(file_name, output_list, encoding_format):
     codecs.register_error('none', lambda e: ('', e.end))
-    with open(f'result/{file_name}.csv', 'w', encoding=encoding_format, errors='none') as f:
+    with open(f'{file_name}.csv', 'w', encoding=encoding_format, errors='none') as f:
         writer = csv.writer(f)
         writer.writerows(output_list)
 
 # JSONファイルの読み込み
-def read_json():
-    pass
+def read_json(file_name):
+    json_open = open(f'{file_name}.json', 'r')
+    json_load = json.load(json_open)
+    return json_load
 
 # JSONファイルの書き出し
-def make_json():
-    pass
+def make_json(file_name, output_dict):
+    with open(f'{file_name}.json', 'w') as f:
+        json.dump(output_dict, f, indent=2, ensure_ascii=False)
+
 
--- dashboard/src/config/develop/update_city_json.py
+++ dashboard/src/config/develop/update_city_json.py
@@ -3,20 +3,34 @@
 import file_handling
 
 # ファイルパス
-PREF_CSV_PATH = ''
-CITY_CSV_PATH = ''
-PREF_JSON_PATH = ''
+PREF_CSV_PATH = 'csv/都道府県'
+CITY_CSV_PATH = 'csv/市町村'
+PREF_JSON_PATH = '../都道府県市区町村'
+RESULT = 'result/都道府県市区町村_'
 
 # ファイルの読み込み
-pref_csv = file_handling.read_csv(PREF_CSV_PATH, 'utf-8')
-city_csv = file_handling.read_csv(CITY_CSV_PATH, 'utf-8')
+pref_csv = file_handling.read_csv(PREF_CSV_PATH, 'utf_8')
+city_csv = file_handling.read_csv(CITY_CSV_PATH, 'utf_8')
 pref_dict = file_handling.read_json(PREF_JSON_PATH)
 
+pref_list = [pref_csv[i+1][1] for i in range(47)]
+ruby_dict = {}
+
 # 都道府県ごとに
+line_count = 1
 for i in range(47):
-    # city_csvを都道府県ごとに分割
+    # city_csvを都道府県ごとに分割して辞書にまとめる
+    ruby_dict[pref_list[i]] = []
+    while line_count < len(city_csv) and city_csv[line_count][1] == pref_list[i]:
+        if city_csv[line_count][4] == '':
+            ruby_dict[pref_list[i]].append(
+                [city_csv[line_count][2], city_csv[line_count][3]])
+        else:
+            ruby_dict[pref_list[i]].append(
+                [city_csv[line_count][4], city_csv[line_count][5]])
+        line_count += 1
 
-
+'''
 # 都道府県ごとに
 for i in range(47):
     # JSONに含まれる市町村名とそのふりがなのセットを作成（形式要検討）
@@ -27,4 +41,7 @@ for i in range(47):
 
     # 書き出し用の辞書の配列を更新
 
+
 # JSONの書き出し
+'''
+
