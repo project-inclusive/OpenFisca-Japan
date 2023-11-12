a6a1a176b9f82022297a7a56f7f3d6a8fe8def0b
mu-perori <mu17perori@gmail.com>
2023-11-13 00:12:29 +0900

    Update municipalities_order.py

--- tools/municipalities_order/municipalities_order.py
+++ tools/municipalities_order/municipalities_order.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3.8
 
+import json
 import pandas as pd
 from pandas import DataFrame
 
-import file_handling
+def read_json(file_name):
+    json_open = open(file_name, 'r')
+    json_load = json.load(json_open)
+    return json_load
+
+def export_json(file_name, output_dict):
+    with open(file_name, 'w') as f:
+        json.dump(output_dict, f, indent=2, ensure_ascii=False)
 
 # ファイルパス
-PREF_CSV_PATH = 'csv/都道府県.csv'
-MUNIC_CSV_PATH = 'csv/市町村.csv'
+PREF_CSV_PATH = 'csv/都道府県'
+MUNIC_CSV_PATH = 'csv/市町村'
 MUNIC_JSON_PATH = '../../dashboard/src/config/都道府県市区町村'
 RESULT = 'result/都道府県市区町村_50音順'
 
 # ファイルの読み込み
-pref_csv = pd.read_csv(PREF_CSV_PATH)
-munic_csv = pd.read_csv(MUNIC_CSV_PATH)
-munic_dict = file_handling.read_json(MUNIC_JSON_PATH)
+pref_csv = pd.read_csv(f'{PREF_CSV_PATH}.csv')
+munic_csv = pd.read_csv(f'{MUNIC_CSV_PATH}.csv')
+munic_json = read_json(f'{MUNIC_JSON_PATH}.json')
 
 
 pref_df = pref_csv[['都道府県']]
@@ -22,66 +30,30 @@ ruby_dict = {}
 
 output_dict = {}
 
-# print(pref_df.head())
-
+# munic_csvの市区町村がNaNだったら政令市･郡･支庁･振興局等を参照して埋める
+munic_csv = munic_csv[['都道府県', '政令市･郡･支庁･振興局等', '市区町村', '政令市･郡･支庁･振興局等（ふりがな）', '市区町村（ふりがな）']]
+munic_csv = munic_csv.fillna(method='ffill', axis=1)
 
 # 都道府県ごとに
-    # munic_csvから該当都道府県の行を抽出
-    # NaNだったら隣を参照して埋める
-    # 必要な列だけにする(ruby_df)
-    # munic_dictの該当都道府県のリストをmunic_dfに変換
+for i in range(47):
+    pref_name = str(pref_df[i:i+1]['都道府県']).split('    ')[1].split('\n')[0]
+
+    # munic_csvから該当都道府県の行を抽出し、必要な列だけにする
+    ruby_df = munic_csv[munic_csv['都道府県'] == pref_name][['市区町村', '市区町村（ふりがな）']]
+
+    # munic_jsonの該当都道府県のリストをmunic_dfに変換
+    munic_df = DataFrame(munic_json[pref_name], columns = ['市区町村'])
+
     # JSONに含まれる市町村名とそのふりがなのdfをマージ
+    munic_ruby_df = pd.merge(munic_df, ruby_df, on = '市区町村', how = 'left')
+
     # ふりがなでソート
-    # 市町村名のみの配列に変換し、書き出し用の辞書に追加
-
-
-'''
-# ファイルパス
-PREF_CSV_PATH = 'csv/都道府県'
-CITY_CSV_PATH = 'csv/市町村'
-CITY_JSON_PATH = '../都道府県市区町村'
-RESULT = 'result/都道府県市区町村_50音順'
-
-# ファイルの読み込み
-pref_csv = file_handling.read_csv(PREF_CSV_PATH, 'utf_8')
-city_csv = file_handling.read_csv(CITY_CSV_PATH, 'utf_8')
-city_dict = file_handling.read_json(CITY_JSON_PATH)
-
-pref_list = [pref_csv[i+1][1] for i in range(47)]
-ruby_dict = {}
-
-output_dict = {}
-
-# 都道府県ごとに
-line_count = 1
-for i in range(47):
-    # city_csvを都道府県ごとに分割して辞書にまとめる
-    ruby_dict[pref_list[i]] = []
-    while line_count < len(city_csv) and city_csv[line_count][1] == pref_list[i]:
-        if city_csv[line_count][4] == '':
-            ruby_dict[pref_list[i]].append(
-                [city_csv[line_count][2], city_csv[line_count][3]])
-        else:
-            ruby_dict[pref_list[i]].append(
-                [city_csv[line_count][4], city_csv[line_count][5]])
-        line_count += 1
-
-# 都道府県ごとに
-for i in range(47):
-    pref_name = pref_list[i]
-    city_df = DataFrame(city_dict[pref_name], columns = ['city'])
-    ruby_df = DataFrame(ruby_dict[pref_name], columns = ['city', 'ruby'])
+    munic_ruby_df = munic_ruby_df.sort_values('市区町村（ふりがな）')
     
-    # JSONに含まれる市町村名とそのふりがなのdfをマージ
-    city_ruby_df = pd.merge(city_df, ruby_df, on = 'city', how = 'left')
-
-    # ふりがなでソート
-    city_ruby_df = city_ruby_df.sort_values('ruby')
+    print(f'{munic_ruby_df}\n')
 
     # 市町村名のみの配列に変換し、書き出し用の辞書に追加
-    output_dict[pref_name] = city_ruby_df['city'].values.tolist()
-
+    output_dict[pref_name] = munic_ruby_df['市区町村'].values.tolist()
 
 # JSONの書き出し
-file_handling.make_json(RESULT, output_dict)
-'''
+export_json(f'{RESULT}.json', output_dict)
