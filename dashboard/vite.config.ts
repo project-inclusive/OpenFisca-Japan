1ce7927a107a00a48b85590de60c29eb4fe2ebc2
Kazuya AZEGAMI <k-azegami@youthfuldays.co.jp>
2024-01-16 21:57:46 +0900

    コメントを修正した

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default () => {
-  // index.htmlの「%=」「%」で囲われた部分を環境変数値で置換する
+  // index.htmlの「%=」「%」で囲われた部分を環境変数値で置換するための処理
   //   例: <title>%=ENV_NAME%</title> -> <title>ENV_NAME</title>
   const env = { ...process.env };
-  console.log('ENV_NAMES:' + Object.keys(env));
-  console.log('YADOKARI_GTM_ID:' + env['YADOKARI_GTM_ID']);
+  // console.log('YADOKARI_GTM_ID:' + env['YADOKARI_GTM_ID']); // デバッグ用
   const htmlPlugin = () => ({
     name: 'html-transform',
     transformIndexHtml: (html: string): string =>
