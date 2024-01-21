de4f22f9eaecadf691345531b21aa593b309928f
syuparn <s.hello.spagetti@gmail.com>
2024-01-22 08:43:22 +0900

    福祉費の災害関連条件を追加

--- openfisca_japan/variables/福祉/生活福祉資金貸付制度.py
+++ openfisca_japan/variables/福祉/生活福祉資金貸付制度.py
@@ -120,13 +120,50 @@ class 福祉費(Variable):
     label = "福祉費"
     reference = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/hukushi_kaigo/seikatsuhogo/seikatsu-fukushi-shikin1/index.html"
 
+    def formula(対象世帯, 対象期間, _parameters):
+        被災している = 対象世帯("被災している", 対象期間)
+        福祉費_災害 = 対象世帯("福祉費_災害", 対象期間)
+        福祉費_災害以外 = 対象世帯("福祉費_災害以外", 対象期間)
+
+        # 被災している場合は災害関連の条件を優先して計算
+        return np.where(被災している, 福祉費_災害, 福祉費_災害以外)
+
+
+class 福祉費_災害(Variable):
+    value_type = float
+    entity = 世帯
+    definition_period = DAY
+    label = "福祉費（災害を受けたことにより臨時に必要となる経費）"
+    reference = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/hukushi_kaigo/seikatsuhogo/seikatsu-fukushi-shikin1/index.html"
+    documentation = """
+    福祉費の用途ごとの条件、金額はこちらを参照。
+    https://www.mhlw.go.jp/bunya/seikatsuhogo/fukushihi.html
+    """
+
+    def formula(対象世帯, 対象期間, parameters):
+        貸付上限額 = parameters(対象期間).福祉.生活福祉資金貸付制度.福祉費_災害_貸付上限額
+        return 貸付上限額
+
+
+# TODO: 用途ごとに条件と金額を細かく出し分ける
+class 福祉費_災害以外(Variable):
+    value_type = float
+    entity = 世帯
+    definition_period = DAY
+    label = "福祉費（災害以外の場合）"
+    reference = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/hukushi_kaigo/seikatsuhogo/seikatsu-fukushi-shikin1/index.html"
+    documentation = """
+    福祉費の用途ごとの条件、金額はこちらを参照。
+    https://www.mhlw.go.jp/bunya/seikatsuhogo/fukushihi.html
+    """
+
     def formula(対象世帯, 対象期間, parameters):
         住民税非課税世帯 = 対象世帯("住民税非課税世帯", 対象期間)  # openfisca_japan/variables/所得.py の「住民税非課税世帯」を参照している
         障害者手帳を持つ世帯員がいる = 対象世帯("障害者手帳を持つ世帯員がいる", 対象期間)
         六十五歳以上の世帯員がいる = 対象世帯("六十五歳以上の世帯員がいる", 対象期間)
 
         # openfisca_japan/parameters/福祉/生活福祉資金貸付制度/福祉費_貸付上限額.yaml を参照している
-        貸付条件 = 住民税非課税世帯 or 障害者手帳を持つ世帯員がいる or 六十五歳以上の世帯員がいる
+        貸付条件 = 住民税非課税世帯 + 障害者手帳を持つ世帯員がいる + 六十五歳以上の世帯員がいる
         福祉費_貸付上限額 = parameters(対象期間).福祉.生活福祉資金貸付制度.福祉費_貸付上限額
 
         return 貸付条件 * 福祉費_貸付上限額
