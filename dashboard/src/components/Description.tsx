ecb12ba78b1e07c0b2ab2f8688e101d8c0210cfd
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2024-01-29 22:05:51 +0900

    トップページの一番上にアプリの説明を追加

--- dashboard/src/components/Description.tsx
+++ dashboard/src/components/Description.tsx
@@ -73,15 +73,7 @@ function Description() {
           />
         )}
 
-        <Center
-          fontSize={configData.style.subTitleFontSize}
-          mb="0.5em"
-          color="cyan.800"
-        >
-          <CheckCircleIcon color="cyan.600" mr="0.3em" />
-          {configData.description.feature[0]}
-        </Center>
-        <Box fontSize={configData.style.descriptionFontSize} mb="1.5em">
+        <Box fontSize={configData.style.descriptionFontSize} mb="1em">
           {configData.description.description[0]}
         </Box>
 
@@ -91,7 +83,7 @@ function Description() {
           color="cyan.800"
         >
           <CheckCircleIcon color="cyan.600" mr="0.3em" />
-          {configData.description.feature[1]}
+          {configData.description.feature[0]}
         </Center>
         <Box fontSize={configData.style.descriptionFontSize} mb="1.5em">
           {configData.description.description[1]}
@@ -103,7 +95,7 @@ function Description() {
           color="cyan.800"
         >
           <CheckCircleIcon color="cyan.600" mr="0.3em" />
-          {configData.description.feature[2]}
+          {configData.description.feature[1]}
         </Center>
         <Box fontSize={configData.style.descriptionFontSize} mb="1.5em">
           {configData.description.description[2]}
@@ -115,12 +107,24 @@ function Description() {
           color="cyan.800"
         >
           <CheckCircleIcon color="cyan.600" mr="0.3em" />
+          {configData.description.feature[2]}
+        </Center>
+        <Box fontSize={configData.style.descriptionFontSize} mb="1.5em">
+          {configData.description.description[3]}
+        </Box>
+
+        <Center
+          fontSize={configData.style.subTitleFontSize}
+          mb="0.5em"
+          color="cyan.800"
+        >
+          <CheckCircleIcon color="cyan.600" mr="0.3em" />
           {configData.description.feature[3]}
         </Center>
         <Box fontSize={configData.style.descriptionFontSize} mb="0.5em">
-          {configData.description.description[3]}
-          <br></br>
           {configData.description.description[4]}
+          <br></br>
+          {configData.description.description[5]}
         </Box>
 
         {/* 見積もり対象制度一覧 */}
