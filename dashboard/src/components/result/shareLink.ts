cf9ad9d1dda54757fca765e714fdd2e2d04eec26
minagishl <mvxd568.co@icloud.com>
2024-03-24 04:28:56 +0900

    JSDOC を追加

--- dashboard/src/components/result/shareLink.ts
+++ dashboard/src/components/result/shareLink.ts
@@ -1,6 +1,10 @@
 import pako from 'pako';
 
-// JSON文字列を圧縮する関数
+/**
+ * Deflates a JSON string using the pako library and encodes it in base64.
+ * @param jsonString - The JSON string to deflate and encode.
+ * @returns The deflated and base64-encoded string.
+ */
 export function deflate(jsonString: string): string {
   const utf8Bytes = new TextEncoder().encode(jsonString);
   const compressedBytes = pako.deflate(utf8Bytes, {
@@ -10,7 +14,11 @@ export function deflate(jsonString: string): string {
   return window.btoa(String.fromCharCode(...compressedBytes));
 }
 
-// 圧縮された文字列を解凍してJSON文字列に戻す関数
+/**
+ * Inflates a compressed string using the pako library.
+ * @param compressedString The compressed string to be inflated.
+ * @returns The inflated string.
+ */
 export function inflate(compressedString: string): string {
   const compressedBytes = Uint8Array.from(window.atob(compressedString), (c) =>
     c.charCodeAt(0)
@@ -19,6 +27,14 @@ export function inflate(compressedString: string): string {
   return new TextDecoder().decode(inflatedBytes);
 }
 
+/**
+ * Generates a short link for sharing calculation results.
+ *
+ * @param obj - object.
+ * @param isSimpleCalculation - Indicates whether it is a simple calculation.
+ * @param isDisasterCalculation - Indicates whether it is a disaster calculation.
+ * @returns The generated short link.
+ */
 export default function shortLink(
   obj: object,
   isSimpleCalculation: boolean,
@@ -31,6 +47,10 @@ export default function shortLink(
   }&2=${isDisasterCalculation ? 1 : 0}`;
 }
 
+/**
+ * Retrieves the calculation type based on the URL parameters.
+ * @returns An object containing the calculation type information.
+ */
 export function calculationType() {
   const urlParams = new URLSearchParams(window.location.search);
   const isSimpleCalculation = urlParams.get('1') === '1';
