c73df13ac4eecd3596b36471eddb5213b1fc37e5
syuparn <s.hello.spagetti@gmail.com>
2024-06-23 10:21:47 +0900

    HIV関連情報のデフォルト値を設定

--- openfisca_japan/variables/福祉/感染症/HIV関連情報.py
+++ openfisca_japan/variables/福祉/感染症/HIV関連情報.py
@@ -10,6 +10,7 @@ from openfisca_japan.entities import 人物
 
 class HIV感染者である(Variable):
     value_type = bool
+    default_value = False
     entity = 人物
     definition_period = DAY
     label = "HIV感染者である"
@@ -17,6 +18,7 @@ class HIV感染者である(Variable):
 
 class 血液製剤の投与によってHIVに感染した(Variable):
     value_type = bool
+    default_value = False
     entity = 人物
     definition_period = DAY
     label = "血液凝固因子製剤の投与によってHIVに感染した"
@@ -24,6 +26,7 @@ class 血液製剤の投与によってHIVに感染した(Variable):
 
 class 家族に血液製剤によるHIV感染者がいる(Variable):
     value_type = bool
+    default_value = False
     entity = 人物
     definition_period = DAY
     label = "家族に血液凝固因子製剤によるHIV感染者がいる"
