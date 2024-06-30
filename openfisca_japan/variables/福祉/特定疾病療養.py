62643e31f032e1127966ef871e3d6fd6562ce7d0
syuparn <s.hello.spagetti@gmail.com>
2024-06-30 14:53:48 +0900

    特定疾病療養のバックエンドを実装

---
+++ openfisca_japan/variables/福祉/特定疾病療養.py
@@ -0,0 +1,35 @@
+"""
+特定疾病療養の実装
+"""
+
+from openfisca_core.periods import DAY
+from openfisca_core.variables import Variable
+from openfisca_japan.entities import 人物
+
+
+class 特定疾病療養の対象者である(Variable):
+    value_type = int
+    entity = 人物
+    definition_period = DAY
+    label = "特定疾病療養の対象者であるか否か"
+    reference = "https://www.kyoukaikenpo.or.jp/shibu/shizuoka/cat080/20130225001/"
+    documentation = """
+    自己負担限度額を定める制度であり他制度の計算結果と合計できないため、対象者であるかどうかのみ計算
+
+    条件は以下リンクも参考になる。
+    https://hyogo-kenchiku-kenpo.or.jp/wp-content/Document/特定疾病療養留意事項.pdf
+    """
+
+    def formula(対象人物, 対象期間, _parameters):
+        血友病の可能性がある = 対象人物("血友病の可能性がある", 対象期間)
+        血友病関連条件 = 血友病の可能性がある
+
+        HIV感染者である = 対象人物("HIV感染者である", 対象期間)
+        血液製剤の投与によってHIVに感染した = 対象人物("血液製剤の投与によってHIVに感染した", 対象期間)
+        HIV関連条件 = HIV感染者である * 血液製剤の投与によってHIVに感染した
+
+        慢性腎不全である = 対象人物("慢性腎不全である", 対象期間)
+        人工透析を行っている = 対象人物("人工透析を行っている", 対象期間)
+        腎不全関連条件 = 慢性腎不全である * 人工透析を行っている
+
+        return 血友病関連条件 + HIV関連条件 + 腎不全関連条件
