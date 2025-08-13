2e86c8df5575c435db4b6d282ad091d7a08edab9
shusuke0812 <shu.ota0812@gmail.com>
2025-08-13 09:55:56 +0900

    SelfPhysicalDisabilityコンポーネントを追加

---
+++ dashboard/src/components/forms/questions/selfPhysicalDisability.tsx
@@ -0,0 +1,35 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { currentDateAtom, householdAtom } from '../../../state';
+import { SelectionQuestion } from '../templates/selectionQuestion';
+
+export const SelfPhysicalDisability = () => {
+  const [household, setHousehold] = useRecoilState(householdAtom);
+  const currentDate = useRecoilValue(currentDateAtom);
+
+  const grades = ['1級', '2級', '3級', '上記以外／持っていない'];
+
+  const selections = grades.map((grade: string) => {
+    return {
+      selection: grade,
+      onClick: () => {
+        const newHousehold = { ...household };
+        newHousehold.世帯員['あなた'].身体障害者手帳等級 = {
+          [currentDate]: grade,
+        };
+        setHousehold({ ...newHousehold});
+      }
+    };
+  });
+
+  return (
+    <SelectionQuestion
+      title="身体障害者手帳を持っていますか？"
+      selections={selections}
+      defaultSelection={({ household }: { household: any }) =>
+        household.世帯員['あなた'].身体障害者手帳等級
+          ? household.世帯員['あなた'].身体障害者手帳等級[currentDate]
+          : null
+      }
+    />
+  );
+};
