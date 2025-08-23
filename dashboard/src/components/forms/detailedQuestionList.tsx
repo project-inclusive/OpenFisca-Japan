6ee975f06d0657756c65912ba505b726284c145c
shusuke0812 <shu.ota0812@gmail.com>
2025-08-23 21:49:57 +0900

    ChildNursingHomeを追加

--- dashboard/src/components/forms/detailedQuestionList.tsx
+++ dashboard/src/components/forms/detailedQuestionList.tsx
@@ -38,6 +38,7 @@ import { ChildCerebralParalysis } from './questions/childCerebralParalysis';
 import { ParentCerebralParalysis } from './questions/parentCerebralParalysis';
 import { SelfNursingHome } from './questions/selfNursingHome';
 import { SpouseNursingHome } from './questions/spouseNursingHome';
+import { ChildNursingHome } from './questions/childNursingHome';
 
 // NOTE: プログレスバーの計算のために設問に順序関係を定義する必要があるため、objectではなくarrayを使用
 // HACK: componentをarray内に定義する際にkeyが必要なため定義している
@@ -469,7 +470,7 @@ const questions = {
     },
     {
       title: '介護施設',
-      component: <DummyQuestion key={32} />,
+      component: <ChildNursingHome key={32} />,
     },
   ],
   親: [
