d987617624846646eebf95ca8b8d7d48d2b72562
shusuke0812 <shu.ota0812@gmail.com>
2025-08-24 10:26:18 +0900

    家を借りたい画面で選択状態を保持するように修正

--- dashboard/src/components/forms/questions/selfRentingHouse.tsx
+++ dashboard/src/components/forms/questions/selfRentingHouse.tsx
@@ -1,9 +1,16 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { YesNoQuestion } from '../templates/yesNoQuestion';
-import { currentDateAtom, householdAtom } from '../../../state';
+import {
+  currentDateAtom,
+  householdAtom,
+  frontendHouseholdAtom,
+} from '../../../state';
 
 export const SelfRentingHouse = () => {
   const [household, setHousehold] = useRecoilState(householdAtom);
+  const [frontendHousehold, setFrontendHousehold] = useRecoilState(
+    frontendHouseholdAtom
+  );
   const currentDate = useRecoilValue(currentDateAtom);
 
   const yesOnClick = () => {
@@ -13,12 +20,20 @@ export const SelfRentingHouse = () => {
       [currentDate]: null,
     };
     setHousehold(newHousehold);
+
+    const newFrontendHousehold = { ...frontendHousehold };
+    newFrontendHousehold.世帯['家を借りたい'] = true;
+    setFrontendHousehold(newFrontendHousehold);
   };
 
   const noOnClick = () => {
     const newHousehold = { ...household };
     delete newHousehold.世帯一覧.世帯1.住宅入居費;
     setHousehold(newHousehold);
+
+    const newFrontendHousehold = { ...frontendHousehold };
+    newFrontendHousehold.世帯['家を借りたい'] = false;
+    setFrontendHousehold(newFrontendHousehold);
   };
 
   return (
@@ -26,12 +41,12 @@ export const SelfRentingHouse = () => {
       title="家を借りたいですか？"
       yesOnClick={yesOnClick}
       noOnClick={noOnClick}
-      defaultSelection={({ household }: { household: any }) => {
-        return null // TODO: あとで修正
-        // if (household.世帯員[personName]?.内部障害?.[currentDate] != null) {
-        //   return household.世帯員[personName].内部障害[currentDate] === '有';
-        // }
-        // return null;
+      defaultSelection={({ frontendHousehold }: { frontendHousehold: any }) => {
+        console.log('household', household);
+        if (frontendHousehold.世帯['家を借りたい'] != null) {
+          return frontendHousehold.世帯['家を借りたい'];
+        }
+        return null;
       }}
     />
   );
