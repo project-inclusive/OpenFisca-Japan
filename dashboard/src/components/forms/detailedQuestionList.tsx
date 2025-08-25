54db65a8e520fceece3464530e4c3bac2a238e33
shusuke0812 <shu.ota0812@gmail.com>
2025-08-26 08:24:52 +0900

    SpouseStudentを追加

--- dashboard/src/components/forms/detailedQuestionList.tsx
+++ dashboard/src/components/forms/detailedQuestionList.tsx
@@ -44,6 +44,7 @@ import { ParentNursingHome } from './questions/parentNursingHome';
 import { SelfPregnancy } from './questions/selfPregnancy';
 import { SpousePregnancy } from './questions/spousePregnancy';
 import { SelfStudent } from './questions/selfStudent';
+import { SpouseStudent } from './questions/spouseStudent';
 
 // NOTE: プログレスバーの計算のために設問に順序関係を定義する必要があるため、objectではなくarrayを使用
 // HACK: componentをarray内に定義する際にkeyが必要なため定義している
@@ -336,12 +337,16 @@ const questions = {
       component: <SpouseNursingHome key={31} />,
     },
     {
+      title: '学生かどうか',
+      component: <SpouseStudent key={32} />,
+    },
+    {
       title: '妊娠',
-      component: <SpousePregnancy key={32} />, // TODO: 「あなた」「配偶者」いずれか一方のみ妊産婦を選択できるようにしたい
+      component: <SpousePregnancy key={33} />, // TODO: 「あなた」「配偶者」いずれか一方のみ妊産婦を選択できるようにしたい
     },
     {
       title: '配偶者がいるがひとり親に該当',
-      component: <DummyQuestion key={33} />,
+      component: <DummyQuestion key={34} />,
     },
   ],
   子ども: [
---
+++ dashboard/src/components/forms/questions/spouseStudent.tsx
@@ -0,0 +1,5 @@
+import { Student } from './student';
+
+export const SpouseStudent = () => {
+  return <Student personName="配偶者" />;
+};
