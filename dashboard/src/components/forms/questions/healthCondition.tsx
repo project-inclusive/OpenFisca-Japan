6e85091fdf45c7f53f97502379a160d468457264
syuparn <s.hello.spagetti@gmail.com>
2025-10-12 11:12:25 +0900

    血友病の質問で見積もり結果がクラッシュする不具合を修正

--- dashboard/src/components/forms/questions/healthCondition.tsx
+++ dashboard/src/components/forms/questions/healthCondition.tsx
@@ -1,5 +1,10 @@
-import { useRecoilState } from 'recoil';
-import { frontendHouseholdAtom } from '../../../state';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  currentDateAtom,
+  frontendHouseholdAtom,
+  householdAtom,
+  questionKeyAtom,
+} from '../../../state';
 import { MultipleSelectionQuestion } from '../templates/multipleSelectionQuestion';
 import { useEffect } from 'react';
 
@@ -10,9 +15,22 @@ export const HealthCondition = ({
   personName: string;
   updateNextQuestionKey: (frontendHousehold: any) => void;
 }) => {
+  const questionKey = useRecoilValue(questionKeyAtom);
+  const currentDate = useRecoilValue(currentDateAtom);
+  const [household, setHousehold] = useRecoilState(householdAtom);
   const [frontendHousehold, setFrontendHousehold] = useRecoilState(
     frontendHouseholdAtom
   );
+
+  // 感染症関連の質問がスキップされた場合も値が欠けないよう、デフォルト値のfalseを挿入
+  // （当てはまる場合は、後の当該質問でtrueに上書きされる）
+  useEffect(() => {
+    const newHousehold = { ...household };
+    newHousehold.世帯員[personName].HIV感染者である = {
+      [currentDate]: false,
+    };
+    setHousehold({ ...newHousehold });
+  }, [questionKey]);
 
   const selectionValues = ['病気がある', 'けがをしている', '障害がある'];
 
