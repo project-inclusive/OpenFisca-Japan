94896e6260dc3f4a7c775bd98673ce2dae46d38a
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2026-03-08 23:37:17 -0400

    ユーザー入力のvariable`個人事業主の必要経費`を追加し、それが正の場合に個人事業主と判断して収入から給与所得控除額ではなく必要経費を差し引いて所得とするように修正

--- openfisca_japan/variables/所得.py
+++ openfisca_japan/variables/所得.py
@@ -31,8 +31,13 @@ class 所得(Variable):
         # https://www.nta.go.jp/taxes/shiraberu/taxanswer/shotoku/1410.htm
         収入 = 対象人物("収入", 対象期間)
         給与所得控除額 = 対象人物("給与所得控除額", 対象期間)
+        個人事業主である = 対象人物("個人事業主である", 対象期間)
+        個人事業主の必要経費 = 対象人物("個人事業主の必要経費", 対象期間)
+        控除額 = 個人事業主である * 個人事業主の必要経費 + \
+            np.logical_not(個人事業主である) * 給与所得控除額
+
         # 負の数にならないよう、0円未満になった場合は0円に補正
-        return np.clip(収入 - 給与所得控除額, 0.0, None)
+        return np.clip(収入 - 控除額, 0.0, None)
 
 
 class 収入(Variable):
@@ -45,6 +50,33 @@ class 収入(Variable):
     # OpenFisca will spread the yearly 金額 over the days contained in the year.
     set_input = set_input_divide_by_period
     label = "人物の収入"
+
+
+class 個人事業主の必要経費(Variable):
+    value_type = float
+    entity = 人物
+    # 年間必要経費を指す。多岐に渡るがまとめた金額をユーザーに入力してもらう
+    # NOTE: 必要経費自体は1年ごとに定義されるが、特定の日付における各種手当に計算できるように DAY で定義
+    definition_period = DAY
+    # Optional attribute. Allows user to declare this variable for a year.
+    # OpenFisca will spread the yearly 金額 over the days contained in the year.
+    set_input = set_input_divide_by_period
+    label = "個人事業主の必要経費"
+    reference = "https://biz.moneyforward.com/tax_return/basic/12079/"
+
+
+class 個人事業主である(Variable):
+    value_type = bool
+    entity = 人物
+    definition_period = DAY
+    label = "個人事業主であるか否か"
+    reference = "https://www.nta.go.jp/taxes/shiraberu/taxanswer/shotoku/1160.htm"
+    # NOTE: 個人事業主であるか否かは個人事業主の必要経費が0円未満か否かで判断する
+
+    def formula(対象人物, 対象期間, _parameters):
+        個人事業主の必要経費 = 対象人物("個人事業主の必要経費", 対象期間)
+        個人事業主である = 個人事業主の必要経費 > 0
+        return 個人事業主である
 
 
 class 世帯所得(Variable):
