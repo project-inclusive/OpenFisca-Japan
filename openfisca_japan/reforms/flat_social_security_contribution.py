ac983a65512a4d0c5f139cd906aecc2262a641c4
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2026-04-15 22:57:26 +0900

    既存のvariableと紛らわしいクラスがある不使用ファイルを削除

--- openfisca_japan/reforms/flat_social_security_contribution.py
+++
@@ -1,9 +0,0 @@
-"""
-This file defines a reform.
-
-A reform is a set of modifications to be applied to a reference tax and benefit system to carry out experiments.
-
-See https://openfisca.org/doc/key-concepts/reforms.html
-"""
-
-# Import from openfisca-core the Python objects used to code the legislation in OpenFisca
