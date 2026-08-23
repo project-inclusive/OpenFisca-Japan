de77edd84a3873f9ad258d654dc746887a5c36ab
syuparn <s.hello.spagetti@gmail.com>
2026-08-23 09:18:33 +0900

    v2.2.0リリース準備

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")  #
 
 setup(
     name = "OpenFisca-Japan",
-    version = "2.1.2",
+    version = "2.2.0",
     author = "proj-inclusive",
     author_email = "proj.inclusive@gmail.com",
     classifiers = [
